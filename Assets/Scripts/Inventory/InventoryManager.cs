81c4749de35d6f23b40f81ef44052fa9690954a2
MR-ATX\johnw <johnwhitakeraustin@gmail.com>
2022-10-14 14:55:41 -0500

    Xferring to Github to continue work with my tower

---
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager :SingletonMonobehavior<InventoryManager>
+{
+    private Dictionary<int, ItemDetails> itemDetailsDictionary;
+
+    [SerializeField] private SO_ItemList itemList = null;
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        // Create item details dictionary
+        CreateItemDetailsDictionary();
+    }
+
+
+    /// <summary>
+    /// Populates the itemDetailsDictionary from the scriptable objects item list
+    /// </summary>
+    private void CreateItemDetailsDictionary()
+    {
+        itemDetailsDictionary = new Dictionary<int, ItemDetails>();
+
+        foreach (ItemDetails itemDetails in itemList.itemDetails)
+        {
+            itemDetailsDictionary.Add(itemDetails.ItemCode, itemDetails);
+        }
+    }
+
+    /// <summary>
+    /// Returns the itemDetails (from the SO_ItemList) for the itemCode, or null if the item code doesn't exist
+    /// </summary>
+    
+    public ItemDetails GetItemDetails(int itemCode)
+    {
+        ItemDetails itemDetails;
+
+        if (itemDetailsDictionary.TryGetValue(itemCode, out itemDetails))
+        {
+            return itemDetails;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
